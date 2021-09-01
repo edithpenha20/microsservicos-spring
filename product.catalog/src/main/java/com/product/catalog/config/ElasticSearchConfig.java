ea7e9bff83b0893d98338ac2e4e0d1084f9137e7
Edith Penha <75163399+edithpenha20@users.noreply.github.com>
2021-09-01 07:44:42 -0400

    Update ElasticSearchConfig.java

--- product.catalog/src/main/java/com/product/catalog/config/ElasticSearchConfig.java
+++ product.catalog/src/main/java/com/product/catalog/config/ElasticSearchConfig.java
@@ -2,39 +2,26 @@ package com.product.catalog.config;
 
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.support.DefaultConversionService;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
 import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
-//import org.springframework.data.elasticsearch.core.ElasticsearchEntityMapper;
-//import org.springframework.data.elasticsearch.core.EntityMapper;
-import org.springframework.data.elasticsearch.core.ElasticsearchTemplate;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-
-import java.net.UnknownHostException;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.product.catalog.repository")
+@ComponentScan(basePackages = {"one.digitalinnovation.experts.productcatalog"})
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
-
     @Override
     @Bean
     public RestHighLevelClient elasticsearchClient() {
-        ClientConfiguration clientConfiguration = ClientConfiguration.builder()
-                .connectedTo("localhost:9200", "localhost:9300")
+
+        final ClientConfiguration clientConfiguration = ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
                 .build();
+
         return RestClients.create(clientConfiguration).rest();
     }
-
-//    @Bean
-//    @Override
-//    public EntityMapper entityMapper() {
-//        ElasticsearchEntityMapper entityMapper = new ElasticsearchEntityMapper(elasticsearchMappingContext(),
-//                new DefaultConversionService());
-//        entityMapper.setConversions(elasticsearchCustomConversions());
-//
-//        return entityMapper;
-//    }
 }
 
